6ebbce816a6facf8e42068e669ba25dbf0902f06
Fernando Vega <juanv@dodmediagroup.co>
2017-01-25 00:26:20 -0500

    Template juega loteria
    -Template

---
+++ src/js/main.js
@@ -0,0 +1,261 @@
+jQuery(document).ready(function($) {
+
+      var menures = {
+        active: 1,
+        menu: $('#menu')
+    };
+
+    $('#btn-menu').on('click',function(){
+        if (menures.active == 1) {
+            menures.menu.addClass('activar-menu');
+            menures.active = 0;
+        } else {
+            menures.menu.removeClass('activar-menu');
+            menures.active = 1;
+        } 
+    });
+
+   var scene = document.getElementById('scene');
+    var parallax = new Parallax(scene);
+
+  $('.js-resizing').each(function(index, el) {
+        $.resizing($(this));
+    });
+
+
+
+  $('.to-svg').each(function(index, el) {
+        $.imgToSvg($(this));
+    });
+    $('.form__ajax').on('submit', function(event) {
+        event.preventDefault();
+
+        $.activeLoading();
+        $form = $(this);
+
+        $.ajax({
+            url: $form.attr('action'),
+            type: $form.attr('method'),
+            dataType: 'json',
+            data: $form.serialize(),
+        })
+        .done(function(data) {
+            $.showMessage(data.message);
+        })
+        .fail(function() {
+            $.showMessage('Fallo durante la conexión al servidor. Intente mas tarde');
+        })
+        .always(function() {
+            $.deactiveLoading();
+        });
+        
+    });
+
+    $('.fancybox').fancybox();
+   
+});
+ 
+
+
+
+$.resizing = function(element){
+    $(window).resize(function() {
+        var width = element.outerWidth();
+        element.css({
+            "height": width * eval(element.attr('data-resizing'))
+        });
+        if(element.hasClass('panel')){
+            element.parent().find('.panel').css({
+                "height": width * eval(element.attr('data-resizing'))
+            });
+        }
+    }).resize();
+}
+
+$.imgToSvg = function(image){
+    var $img = image;
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        var $svg = jQuery(data).find('svg');
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+        $svg = $svg.removeAttr('xmlns:a');
+        $img.replaceWith($svg);
+
+        if($img.hasClass('map-animate'))
+            $.myScrollAnimate();
+    }, 'xml');
+}
+
+      /*
+       *  Different effects
+       */
+
+      // Change title type, overlay closing speed
+      $(".fancybox-effects-a").fancybox({
+        helpers: {
+          title : {
+            type : 'outside'
+          },
+          overlay : {
+            speedOut : 0
+          }
+        }
+      });
+
+      // Disable opening and closing animations, change title type
+      $(".fancybox-effects-b").fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+
+        helpers : {
+          title : {
+            type : 'over'
+          }
+        }
+      });
+
+      // Set custom style, close if clicked, change title type and overlay color
+      $(".fancybox-effects-c").fancybox({
+        wrapCSS    : 'fancybox-custom',
+        closeClick : true,
+
+        openEffect : 'none',
+
+        helpers : {
+          title : {
+            type : 'inside'
+          },
+          overlay : {
+            css : {
+              'background' : 'rgba(238,238,238,0.85)'
+            }
+          }
+        }
+      });
+
+      // Remove padding, set opening and closing animations, close if clicked and disable overlay
+      $(".fancybox-effects-d").fancybox({
+        padding: 0,
+
+        openEffect : 'elastic',
+        openSpeed  : 150,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 150,
+
+        closeClick : true,
+
+        helpers : {
+          overlay : null
+        }
+      });
+
+      /*
+       *  Button helper. Disable animations, hide close button, change title type and content
+       */
+
+      $('.fancybox-buttons').fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+
+        helpers : {
+          title : {
+            type : 'inside'
+          },
+          buttons : {}
+        },
+
+        afterLoad : function() {
+          this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+        }
+      });
+
+
+      /*
+       *  Thumbnail helper. Disable animations, hide close button, arrows and slide to next gallery item if clicked
+       */
+
+      $('.fancybox-thumbs').fancybox({
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+        arrows    : false,
+        nextClick : true,
+
+        helpers : {
+          thumbs : {
+            width  : 50,
+            height : 50
+          }
+        }
+      });
+
+      /*
+       *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+      */
+      $('.fancybox-media')
+        .attr('rel', 'media-gallery')
+        .fancybox({
+          openEffect : 'none',
+          closeEffect : 'none',
+          prevEffect : 'none',
+          nextEffect : 'none',
+
+          arrows : false,
+          helpers : {
+            media : {},
+            buttons : {}
+          }
+        });
+
+      /*
+       *  Open manually
+       */
+
+      $("#fancybox-manual-a").click(function() {
+        $.fancybox.open('1_b.jpg');
+      });
+
+      $("#fancybox-manual-b").click(function() {
+        $.fancybox.open({
+          href : 'iframe.html',
+          type : 'iframe',
+          padding : 5
+        });
+      });
+
+      $("#fancybox-manual-c").click(function() {
+        $.fancybox.open([
+          {
+            href : '1_b.jpg',
+            title : 'My title'
+          }, {
+            href : '2_b.jpg',
+            title : '2nd title'
+          }, {
+            href : '3_b.jpg'
+          }
+        ], {
+          helpers : {
+            thumbs : {
+              width: 75,
+              height: 50
+            }
+          }
+        });
+      });
+  
